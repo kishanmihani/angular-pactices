e8cef82e33d289c6a1d91c3c6eb6f3a75168532c
Kishan Mihani <kishanmihani918@gmail.com>
2024-05-16 00:28:33 +0530

    marge

--- src/app/progressbar/progressbar.component.spec.ts
+++ src/app/progressbar/progressbar.component.spec.ts
@@ -17,7 +17,7 @@ describe('ProgressbarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', () => { 
     expect(component).toBeTruthy();
   });
 });
